1900f575ba9d8d0cdb154de2462100f89db155d2
nakana <t-nakanishi@esm.co.jp>
2017-04-07 12:25:12 +0900

    機能を追加した。READMEを追記した

--- scripts/foo.js
+++ scripts/foo.js
@@ -1,6 +1,7 @@
 var https = require('https');
+var querystring = require('querystring');
 
-var url = '***********************************';
+var url = 'https://script.google.com/macros/s/***************_***************************************/exec?kind=';
 
 function send(word){
   var targetUrl = url + word;
@@ -12,9 +13,68 @@ function send(word){
   });
 }
 
+
+
+function nstr(n){
+  if(0 <= n && n <=9) return '0' + n;
+  return '' + n;
+}
+
+
+function send2(sender, date, message){
+  var postData = querystring.stringify({
+    'date' : date,
+    'text' : message,
+    'sender' : sender
+  });
+
+  var options = {
+    hostname: 'script.google.com',
+    port: 443,
+    path: '/macros/s/***************_***************************************/exec',
+                      
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  
+  var req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+  
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+  });
+  
+  req.write(postData);
+  req.end();
+}
+
+
+
+
+
 module.exports = function(robot) {
-  robot.respond(/hi/i, function(msg) {
-    msg.send('Hi, はっしー');
+  robot.hear(/********/i, function(msg) {
+    var data = msg.message.data;
+
+    var date = new Date(data.created_at);
+    var dateStr = date.getFullYear() + "/" + nstr(date.getMonth()) + "/" + nstr(date.getDate()) + " " + nstr(date.getHours()) + ":" + nstr(date.getMinutes()) + ":" + nstr(date.getSeconds());
+
+    var sender = data.sender_name;
+    var text = data.body_plain;
+    send2(sender, dateStr, text);
   });
   robot.respond(/IN/i, function(msg) {
     send('IN');
